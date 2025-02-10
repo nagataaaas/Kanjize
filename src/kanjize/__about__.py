e201653587c29fee4d274a6e514751c1df329ba9
nagataaaas <nagata@nagata.pro>
2025-02-10 13:13:35 +0900

    use uv for packaging

---
+++ src/kanjize/__about__.py
@@ -0,0 +1,10 @@
+__title__ = "kanjize"
+__description__ = "Easy converter between Kanji-Number and Integer"
+__url__ = "https://github.com/nagataaaas/kanjize"
+__version_info__ = ("1", "5", "0")
+__version__ = ".".join(__version_info__)
+__author__ = "Yamato Nagata"
+__author_email__ = "nagata@nagata.pro"
+__maintainer__ = "Yamato Nagata"
+__license__ = "MIT"
+__copyright__ = "(c) 2019-2023 by Yamato Nagata"
---
+++ src/kanjize/__init__.py
@@ -0,0 +1,19 @@
+from .__about__ import __version__
+from .kanjize import (
+    number2kanji,
+    Number,
+    kanji2number,
+    KanjizeConfiguration,
+    KanjizeZero,
+    KanjizeStyle,
+)
+
+__all__ = [
+    "__version__",
+    "number2kanji",
+    "kanji2number",
+    "Number",
+    "KanjizeConfiguration",
+    "KanjizeZero",
+    "KanjizeStyle",
+]
